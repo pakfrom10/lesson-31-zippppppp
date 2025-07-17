942d0bb4a90cac462cf634f2def40006e5ef74bb
pakfrom10 <pakfrom10@gmail.com>
2025-07-17 13:46:26 +0400

    dsde

---
+++ main.py
@@ -0,0 +1,7 @@
+#Write a program to return - 1. zipped list from two lists 
+#2. elements of two lists zipped together, but 2nd list in reverse order 
+#3. elements of two lists zipped into a dictionary
+set1=[1,2,3,4,5,6,7,8]
+set2=["a","b","c","d","e","f","g"]
+set3=zip(set1,set2)
+print(list(set3))
